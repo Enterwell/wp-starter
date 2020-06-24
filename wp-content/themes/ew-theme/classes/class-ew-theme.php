6b6b1e7ffde874697450caeb5f3425b322982031
mateikki <matej.bosnjak@enterwell.net>
2020-06-24 10:11:04 +0200

    [Theme] Comments removed and hidden

--- wp-content/themes/ew-theme/classes/class-ew-theme.php
+++ wp-content/themes/ew-theme/classes/class-ew-theme.php
@@ -33,6 +33,25 @@ class Ew_Theme {
 		// Load theme view models
 		$vm_loader = new EW_View_Models_Loader();
 		$vm_loader->load_theme_view_models();
+
+		#region Remove comments support
+		// Removes from admin menu
+		add_action( 'admin_menu', function () {
+			remove_menu_page( 'edit-comments.php' );
+		} );
+
+		// Removes from post and pages
+		add_action( 'init', function () {
+			remove_post_type_support( 'post', 'comments' );
+			remove_post_type_support( 'page', 'comments' );
+		}, 100 );
+
+		// Removes from admin bar
+		add_action( 'wp_before_admin_bar_render', function () {
+			global $wp_admin_bar;
+			$wp_admin_bar->remove_menu( 'comments' );
+		} );
+		#endregion
 	}
 
 	/**
