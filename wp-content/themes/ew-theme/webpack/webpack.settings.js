e3cd214dedb2c91f263e8dd3f95328ae17f1deae
andjela.rotim <andjela.rotim@enterwell.net>
2022-11-24 15:45:54 +0100

    Changed webpack settings and added browser-sync

--- wp-content/themes/ew-theme/webpack/webpack.settings.js
+++ wp-content/themes/ew-theme/webpack/webpack.settings.js
@@ -1,6 +1,38 @@
 const themeConfig = require('../theme-config');
 
 const path = require('path');
+var os = require('os');
+
+/**
+ * Gets the local IP address or 127.0.0.1
+ * It returns the first address that starts with 192.168.0. or 192.168.1.
+ * @returns string
+ */
+function getLocalIp() {
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+
+  // Iterates over the addreses
+  for (const address of addresses) {
+    if (
+      address.startsWith('192.168.0.') ||
+      address.startsWith('192.168.1.')
+    ) {
+      return address;
+    }
+  }
+
+  // Default - returns home address
+  return '127.0.0.1';
+}
 
 const PATHS = {
   root: path.join(__dirname, '../'),
@@ -11,16 +43,18 @@ const PATHS = {
   build: path.join(__dirname, '../assets/dist'),
   gutenberg: path.join(__dirname, '../assets/gutenberg')
 };
+
 /**
  * Settings for webpack dev server.
  *
  * @link https://webpack.js.org/configuration/dev-server/
  * @type {{port: number, address: string}}
  */
+const MY_IP = getLocalIp();
 const WebpackDevServerSettings = {
   port: themeConfig.webpackPort,
-  host: themeConfig.webpackHost,
-  address: 'http://' + themeConfig.webpackHost + ':' + themeConfig.webpackPort + '/',
+  host: '0.0.0.0',
+  address: 'http://' + MY_IP + ':' + themeConfig.webpackPort + '/',
 };
 
 /**
